7dbd8a2c48513fba05c80f40a3b4fd9fc641b9c9
SebastianMarius <sebastianmarius2100@gmail.com>
2022-03-27 04:12:24 +0300

    Update Game.jsx

--- src/routes/Game.jsx
+++ src/routes/Game.jsx
@@ -3,7 +3,7 @@ import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import {MdCatchingPokemon} from "react-icons/md";
+import { MdCatchingPokemon } from 'react-icons/md';
 
 const Game = () => {
     const handleSubmit = (event) => {
@@ -11,25 +11,30 @@ const Game = () => {
     };
 
     return (
-        <Box sx={{display: "flex", minHeight: "100vh", width: "100%", alignItems: "center"}}>
-            <Container component="main" maxWidth="lg">
+        <Box
+            sx={{
+                display: 'flex',
+                minHeight: '100vh',
+                width: '100%',
+                alignItems: 'center',
+            }}>
+            <Container component='main' maxWidth='lg'>
                 <Box
                     sx={{
                         display: 'flex',
                         flexDirection: 'column',
                         alignItems: 'center',
-                    }}
-                >
-                    <Avatar sx={{m: 1, bgcolor: 'primary.main'}}>
-                        <MdCatchingPokemon/>
+                    }}>
+                    <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
+                        <MdCatchingPokemon />
                     </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Pokemon silhouette goes here
+                    <Typography component='h1' variant='h5'>
+                        Pokemon ssssilhouette goes here
                     </Typography>
                 </Box>
             </Container>
         </Box>
     );
-}
+};
 
 export default Game;
