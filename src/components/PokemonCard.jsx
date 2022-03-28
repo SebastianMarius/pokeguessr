e29bad55bf2a725d4614756864eb166d44327849
cucgabrielpaul~ <cucgabrielpaul@yahoo.com~>
2022-03-28 18:01:45 +0300

    Styled pokemon card

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -12,16 +12,18 @@ import Box from "@mui/material/Box";
 import { getPokemonTypeColors, typeToColor } from "../utils/pokemonUtils";
 import { getLinearGradientFromColors } from "../utils/utils";
 import Input from "@mui/material/Input";
-import Button from '@mui/material/Button';
-import { bgcolor } from "@mui/system";
+import Button from "@mui/material/Button";
 
 // functie dau ca param boolean daca i corect sau nu
 const PokemonCard = ({ pokemon, onGuessPokemon }) => {
   const [combinedColor, setCombinedColor] = useState(
     getLinearGradientFromColors(getPokemonTypeColors(pokemon.types))
   );
-  const [pokemonName, setPokemonName]= useState();
-
+  const [pokemonName, setPokemonName] = useState();
+  const onSubmit = () => {
+    onGuessPokemon(pokemonName.toLowerCase() === pokemon.name.toLowerCase());
+    setPokemonName("");
+  };
   useEffect(() => {
     setCombinedColor(
       getLinearGradientFromColors(getPokemonTypeColors(pokemon.types))
@@ -74,51 +76,47 @@ const PokemonCard = ({ pokemon, onGuessPokemon }) => {
             component="h1"
             variant="h3"
             sx={{ fontWeight: "bold", margin: "0.5rem" }}
+            textAlign="center"
           >
             Who's that Pokemon?
           </Typography>
-          <Typography gutterBottom variant="h5" component="div">
-            {/* {pokemon.name} */}
-            {console.log(pokemon)}
+          <Box
+            width={"100%"}
+            display={"flex"}
+            justifyContent={"center"}
+            margin={"15px 0"}
+          >
             <Input
               className="pokemon_input"
               placeholder="Pokemon name"
               sx={{
+                width: "90%",
                 borderColor: typeToColor(pokemon.types[0]),
-                mr:5
-                
               }}
               value={pokemonName}
-
-              onKeyDown={(e)=>{
-                if(e.code==="Enter"){
-                    onGuessPokemon(pokemonName === pokemon.name);
-                    setPokemonName('');
+              onKeyDown={(e) => {
+                if (e.code === "Enter") {
+                  onSubmit();
                 }
-            }}
-            
-              onChange={(e)=>{setPokemonName(e.target.value)}}
+              }}
+              onChange={(e) => {
+                setPokemonName(e.target.value);
+              }}
             />
-            <Button variant="contained"
-            
-                sx={{
-                    bgcolor:typeToColor(pokemon.types[0]),
-                    borderRadius:2
-                    }} 
-                
-              
-
-                
-                onClick={()=>{  
-                  console.log("test")
-                    onGuessPokemon(pokemonName === pokemon.name);
-                    setPokemonName('');
-                    
-                }}
-            
-            >Send</Button>
-
-          </Typography>
+          </Box>
+          <Button
+            variant="contained"
+            sx={{
+              bgcolor: typeToColor(pokemon.types[0]),
+              borderRadius: "25px",
+              padding: "8px 30px 6px 30px",
+            }}
+            onClick={() => {
+              onSubmit();
+            }}
+          >
+            Guess
+          </Button>
         </Box>
       </CardContent>
     </Card>
